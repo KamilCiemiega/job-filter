70f3a7f62c87bdda905f5875e3212cf6df007ec3
unknown <Administrator@IT-CIEMIEGAK.brwsa.com.pl>
2022-03-09 14:35:27 +0100

    changes

--- src/components/FilterElement.js
+++ src/components/FilterElement.js
@@ -1,5 +1,22 @@
 const FilterElement = (props) => {
-  return <div>{props.company}</div>;
+  return (
+    <div className="main">
+      <div className="main-logo"> {props.logo}</div>
+      <div>
+        <div>
+          <div className="main">{props.company}</div>
+          {props.new ? <div className="main-new">NEW!</div> : null}
+          {props.featured ? <div className="main-new">FEATURED</div> : null}
+        </div>
+        <div>{props.position}</div>
+        <div>
+          <div>{props.postedAt}</div>
+          <div>{props.contract}</div>
+          <div>{props.location}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FilterElement;
